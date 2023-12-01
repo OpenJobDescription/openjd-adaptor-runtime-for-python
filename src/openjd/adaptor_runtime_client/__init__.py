2766d8b2c2275dd5928ccf921f548e07116e4fb5
Hongli Chen <honglich@amazon.com>
2023-12-01 20:03:51 +0000

    refactor: Extract the common code from client_interface.py to the base_client_interface.py.
    Signed-off-by: Hongli Chen <honglich@amazon.com>

--- src/openjd/adaptor_runtime_client/__init__.py
+++ src/openjd/adaptor_runtime_client/__init__.py
@@ -1,10 +1,11 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
 from .action import Action
-from .client_interface import (
+from .posix_client_interface import (
     HTTPClientInterface,
-    PathMappingRule,
 )
+
+from .base_client_interface import PathMappingRule
 
 __all__ = [
     "Action",
---
+++ src/openjd/adaptor_runtime_client/posix_client_interface.py
@@ -0,0 +1,61 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+from __future__ import annotations
+
+import signal as _signal
+from .base_client_interface import Response as _Response
+from typing import Dict as _Dict
+
+from .base_client_interface import BaseClientInterface
+from .connection import UnixHTTPConnection as _UnixHTTPConnection
+from urllib.parse import urlencode as _urlencode
+
+
+# Set timeout to None so our requests are blocking calls with no timeout.
+# See socket.settimeout
+_REQUEST_TIMEOUT = None
+
+
+class HTTPClientInterface(BaseClientInterface):
+    socket_path: str
+
+    def __init__(self, socket_path: str) -> None:
+        """When the client is created, we need the port number to connect to the server.
+
+        Args:
+            socket_path (str): The path to the UNIX domain socket to use.
+        """
+        super().__init__()
+        self.socket_path = socket_path
+        # NOTE: The signals SIGKILL and SIGSTOP cannot be caught, blocked, or ignored.
+        # Reference: https://man7.org/linux/man-pages/man7/signal.7.html
+        # SIGTERM graceful shutdown.
+        _signal.signal(_signal.SIGTERM, self.graceful_shutdown)
+
+    def _send_request(
+        self, method: str, request_path: str, *, query_string_params: _Dict | None = None
+    ) -> _Response:
+        """
+        Send a request to the server and return the response.
+
+        Args:
+            method (str): The HTTP method, e.g. 'GET', 'POST'.
+            request_path (str): The path for the request.
+            query_string_params (_Dict | None, optional): Query string parameters to include in the request.
+                Defaults to None. In Linux, the query string parameters will be added to the URL
+
+        Returns:
+            Response: The response from the server.
+        """
+        headers = {
+            "Content-type": "application/json",
+        }
+        connection = _UnixHTTPConnection(self.socket_path, timeout=_REQUEST_TIMEOUT)
+        if query_string_params:
+            request_path += "?" + _urlencode(query_string_params)
+        connection.request(method, request_path, headers=headers)
+        response = connection.getresponse()
+        connection.close()
+        length = response.length if response.length else 0
+        body = response.read().decode() if length else ""
+        return _Response(response.status, body, response.reason, length)
