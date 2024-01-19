b13a478de58618941cee08b1055bd669bc4421f8
Robert Luttrell <139398722+roblutt@users.noreply.github.com>
2024-01-19 12:04:18 -0800

    test: Fix configuration manager integ tests on Windows (#55)
    Signed-off-by: Robert Luttrell <roblutt@amazon.com>

--- test/openjd/adaptor_runtime/integ/adaptors/configuration/test_configuration.py
+++ test/openjd/adaptor_runtime/integ/adaptors/configuration/test_configuration.py
@@ -26,43 +26,52 @@ class TestFromFile:
         }
         config = {"key": "value"}
 
-        # fmt: off
-        with tempfile.NamedTemporaryFile(mode="w+") as schema_file, \
-                tempfile.NamedTemporaryFile(mode="w+") as config_file:
-            json.dump(json_schema, schema_file.file)
-            json.dump(config, config_file.file)
-            schema_file.seek(0)
-            config_file.seek(0)
+        try:
+            # GIVEN
+            with tempfile.NamedTemporaryFile(
+                mode="w+", delete=False
+            ) as schema_file, tempfile.NamedTemporaryFile(mode="w+", delete=False) as config_file:
+                json.dump(json_schema, schema_file.file)
+                json.dump(config, config_file.file)
+                schema_file.seek(0)
+                config_file.seek(0)
 
-            # WHEN
-            result = Configuration.from_file(
-                config_path=config_file.name,
-                schema_path=schema_file.name,
-            )
-        # fmt: on
+                # WHEN
+                result = Configuration.from_file(
+                    config_path=config_file.name,
+                    schema_path=schema_file.name,
+                )
 
-        # THEN
-        assert result._config == config
+            # THEN
+            assert result._config == config
 
-        schema_file.close()
-        config_file.close()
+        finally:
+            if os.path.exists(config_file.name):
+                os.remove(config_file.name)
+            if os.path.exists(schema_file.name):
+                os.remove(schema_file.name)
 
     def test_raises_when_config_file_fails_to_open(
         self, tmp_path: _pathlib.Path, caplog: pytest.LogCaptureFixture
     ):
-        # GIVEN
-        with tempfile.NamedTemporaryFile(mode="w+") as schema_file:
-            json.dump({}, schema_file.file)
-            schema_file.seek(0)
-            non_existent_filepath = os.path.join(tmp_path.absolute(), "non_existent_file")
+        try:
+            # GIVEN
+            with tempfile.NamedTemporaryFile(mode="w+", delete=False) as schema_file:
+                json.dump({}, schema_file.file)
+                schema_file.seek(0)
+                non_existent_filepath = os.path.join(tmp_path.absolute(), "non_existent_file")
 
-            # WHEN
-            with pytest.raises(OSError) as raised_err:
-                Configuration.from_file(
-                    schema_path=schema_file.name,
-                    config_path=non_existent_filepath,
-                )
+                # WHEN
+                with pytest.raises(OSError) as raised_err:
+                    Configuration.from_file(
+                        schema_path=schema_file.name,
+                        config_path=non_existent_filepath,
+                    )
 
-        # THEN
-        assert isinstance(raised_err.value, OSError)
-        assert f"Failed to open configuration at {non_existent_filepath}: " in caplog.text
+            # THEN
+            assert isinstance(raised_err.value, OSError)
+            assert f"Failed to open configuration at {non_existent_filepath}: " in caplog.text
+
+        finally:
+            if os.path.exists(schema_file.name):
+                os.remove(schema_file.name)
--- test/openjd/adaptor_runtime/integ/adaptors/configuration/test_configuration_manager.py
+++ test/openjd/adaptor_runtime/integ/adaptors/configuration/test_configuration_manager.py
@@ -12,10 +12,9 @@ import pytest
 from openjd.adaptor_runtime.adaptors.configuration import Configuration, ConfigurationManager
 
 
-@pytest.mark.Linux
-class TestConfigurationManagerLinux:
+class TestConfigurationManager:
     """
-    Linux-specific integration tests for ConfigurationManager
+    Integration tests for ConfigurationManager
     """
 
     @pytest.fixture
@@ -29,32 +28,38 @@ class TestConfigurationManagerLinux:
                 "usrkey": {"type": "string"},
             },
         }
-        with tempfile.NamedTemporaryFile(mode="w+") as schema_file:
+        with tempfile.NamedTemporaryFile(mode="w+", delete=False) as schema_file:
             json.dump(json_schema, schema_file.file)
             schema_file.seek(0)
             yield schema_file
+        os.remove(schema_file.name)
 
     def test_gets_system_config(self, json_schema_file: IO[str]):
         # GIVEN
         config = {"key": "value"}
 
-        with tempfile.NamedTemporaryFile(mode="w+") as config_file:
-            json.dump(config, config_file.file)
-            config_file.seek(0)
-            manager = ConfigurationManager(
-                config_cls=Configuration,
-                schema_path=json_schema_file.name,
-                system_config_path=config_file.name,
-                # These fields can be empty since they will not be used in this test
-                default_config_path="",
-                user_config_rel_path="",
-            )
+        try:
+            with tempfile.NamedTemporaryFile(mode="w+", delete=False) as config_file:
+                json.dump(config, config_file.file)
+                config_file.seek(0)
+                manager = ConfigurationManager(
+                    config_cls=Configuration,
+                    schema_path=json_schema_file.name,
+                    system_config_path=config_file.name,
+                    # These fields can be empty since they will not be used in this test
+                    default_config_path="",
+                    user_config_rel_path="",
+                )
 
-            # WHEN
-            sys_config = manager.get_system_config()
+                # WHEN
+                sys_config = manager.get_system_config()
 
-        # THEN
-        assert sys_config is not None and sys_config._config == config
+            # THEN
+            assert sys_config is not None and sys_config._config == config
+
+        finally:
+            if os.path.exists(config_file.name):
+                os.remove(config_file.name)
 
     def test_builds_config(self, json_schema_file: IO[str]):
         # GIVEN
@@ -66,32 +71,44 @@ class TestConfigurationManagerLinux:
         system_config = {"syskey": "system"}
         user_config = {"usrkey": "user"}
 
-        # fmt: off
         homedir = os.path.expanduser("~")
-        with tempfile.NamedTemporaryFile(mode="w+") as default_config_file, \
-                tempfile.NamedTemporaryFile(mode="w+") as system_config_file, \
-                tempfile.NamedTemporaryFile(mode="w+", dir=homedir) as user_config_file:
-            json.dump(default_config, default_config_file)
-            json.dump(system_config, system_config_file)
-            json.dump(user_config, user_config_file)
-            default_config_file.seek(0)
-            system_config_file.seek(0)
-            user_config_file.seek(0)
 
-            manager = ConfigurationManager(
-                config_cls=Configuration,
-                schema_path=json_schema_file.name,
-                default_config_path=default_config_file.name,
-                system_config_path=system_config_file.name,
-                user_config_rel_path=os.path.relpath(
-                    user_config_file.name,
-                    start=os.path.expanduser("~"),
-                ),
-            )
+        try:
+            with tempfile.NamedTemporaryFile(
+                mode="w+", delete=False
+            ) as default_config_file, tempfile.NamedTemporaryFile(
+                mode="w+", delete=False
+            ) as system_config_file, tempfile.NamedTemporaryFile(
+                mode="w+", dir=homedir, delete=False
+            ) as user_config_file:
+                json.dump(default_config, default_config_file)
+                json.dump(system_config, system_config_file)
+                json.dump(user_config, user_config_file)
+                default_config_file.seek(0)
+                system_config_file.seek(0)
+                user_config_file.seek(0)
 
-            # WHEN
-            result = manager.build_config()
-        # fmt: on
+                manager = ConfigurationManager(
+                    config_cls=Configuration,
+                    schema_path=json_schema_file.name,
+                    default_config_path=default_config_file.name,
+                    system_config_path=system_config_file.name,
+                    user_config_rel_path=os.path.relpath(
+                        user_config_file.name,
+                        start=os.path.expanduser("~"),
+                    ),
+                )
 
-        # THEN
-        assert result._config == {**default_config, **system_config, **user_config}
+                # WHEN
+                result = manager.build_config()
+
+                # THEN
+                assert result._config == {**default_config, **system_config, **user_config}
+
+        finally:
+            if os.path.exists(default_config_file.name):
+                os.remove(default_config_file.name)
+            if os.path.exists(system_config_file.name):
+                os.remove(system_config_file.name)
+            if os.path.exists(user_config_file.name):
+                os.remove(user_config_file.name)
