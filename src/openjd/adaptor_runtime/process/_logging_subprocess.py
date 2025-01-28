687b3848d0b19c0731b72e673360be2578b34b82
Justin Blagden <jblagden@users.noreply.github.com>
2025-01-28 09:22:51 -0600

    Resolves code smells called out by sonarqube
    Fixed an always-true type check
    
    Removed an explicit throw of un-caught exception types

--- src/openjd/adaptor_runtime/process/_logging_subprocess.py
+++ src/openjd/adaptor_runtime/process/_logging_subprocess.py
@@ -78,7 +78,7 @@ class LoggingSubprocess(object):
             exe_path = shutil.which(executable)
 
             # If we didn't find the executable found by which
-            if type(exe_path) is not None:
+            if exe_path is not None:
                 raise FileNotFoundError(
                     f"Could not find adaptor executable at: {exe_path} using alias {executable}\n"
                     f"Error:{fnf_error}"
@@ -89,9 +89,6 @@ class LoggingSubprocess(object):
                 f"Is the executable on the PATH or in the startup directory?\n"
                 f"Error:{fnf_error}"
             )
-
-        except Exception as error:
-            raise error
 
         if not self._process.stdout:  # pragma: no cover
             raise RuntimeError("process stdout not set")
